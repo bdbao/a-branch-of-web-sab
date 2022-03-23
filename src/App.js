bf7b80296e0908a2f98081338f87ed1c5c45eb04
Duy Bảo <91304039+bdbao@users.noreply.github.com>
2022-03-23 16:09:09 +0700

    remove Stashed-chanegs

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-<<<<<<< Updated upstream
-=======
 import HomePage from './Components/HomePage/HomePage';
->>>>>>> Stashed changes
 
 function App() {
 
   return (
-<<<<<<< Updated upstream
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
       <div className="App">
     
       <HomePage />
 
       </div>
->>>>>>> Stashed changes
   );
 }
 
